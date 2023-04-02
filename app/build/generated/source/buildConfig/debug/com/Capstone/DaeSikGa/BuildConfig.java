f3a42942ffd37883a61f6b56afbd4e04353f5606
Jeongthingamajig <netpetet@naver.com>
2023-04-02 14:29:41 +0900

    test

---
+++ app/build/generated/source/buildConfig/debug/com/Capstone/DaeSikGa/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.Capstone.DaeSikGa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.Capstone.DaeSikGa";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
